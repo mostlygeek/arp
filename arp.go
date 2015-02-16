ad86feeaf27111cd6ec35cd56e93c33507c8d758
Benson Wong <mostlygeek+git@gmail.com>
2015-02-16 00:39:57 -0800

    make arp cache concurrency safe

--- arp.go
+++ arp.go
@@ -1,20 +1,46 @@
 package arp
 
+import (
+	"sync"
+)
+
 type ArpTable map[string]string
 
+type cache struct {
+	sync.RWMutex
+	table ArpTable
+}
+
+func (c *cache) Refresh() {
+	c.Lock()
+	defer c.Unlock()
+	c.table = Table()
+}
+
+func (c *cache) Search(ip string) string {
+	c.RLock()
+	defer c.RUnlock()
+
+	mac, ok := c.table[ip]
+
+	if !ok {
+		c.RUnlock()
+		c.Refresh()
+		c.RLock()
+		mac = c.table[ip]
+	}
+
+	return mac
+}
+
 var (
-	cache = make(ArpTable)
+	arpCache = &cache{
+		table: make(ArpTable),
+	}
 )
 
 // Search looks up the MAC address for an IP address
 // in the arp table
 func Search(ip string) string {
-
-	mac, ok := cache[ip]
-	if !ok {
-		cache = Table()  // refresh the cache
-		return cache[ip] // hope that it's there
-	}
-
-	return mac
+	return arpCache.Search(ip)
 }
